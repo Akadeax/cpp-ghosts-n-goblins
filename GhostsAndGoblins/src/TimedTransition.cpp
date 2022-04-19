4f7076c8adcd5a7ef73757c28da6a52ef3fcaf21
Aka deax <patrickqwertz@gmail.com>
2022-04-19 17:58:33 +0200

    Initial rework commit

--- GhostsAndGoblins/src/TimedTransition.cpp
+++
@@ -1,1 +0,0 @@
-#include "TimedTransition.h"
--- GhostsAndGoblins/src/collisions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Vector2f.h"
-
-namespace collisions
-{
-	bool PointRect(Vector2f p, Rectf rect);
-	bool LineLine(Vector2f p1, Vector2f p2, Vector2f p3, Vector2f p4);
-	bool LineRect(Vector2f p1, Vector2f p2, Rectf rect);
-}
